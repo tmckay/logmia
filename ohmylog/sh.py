e519278b39c1538248b812b21fc63313be1bc408
Taylor McKay <taylormckay@gmail.com>
2021-03-04 18:55:07 -0800

    WIP: Add start of sh module

---
+++ ohmylog/sh.py
@@ -0,0 +1,12 @@
+import sys
+
+
+class Sh:
+    """Provides a layer on top of normal shell output. This extra
+    layer allows for manipulating log output on the shell e.g.
+    removing printed output."""
+    def __init__(self, stream=sys.stderr):
+        self.stream = stream
+
+    def print(self, chars):
+        pass
