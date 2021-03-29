667e645881ba9ceecae0487edfdb67e6b1799966
Taylor McKay <taylormckay@gmail.com>
2021-03-28 20:55:52 -0700

    Add a reset line to sh module

--- logmia/sh.py
+++ logmia/sh.py
@@ -7,6 +7,14 @@ class Sh:
     removing printed output."""
     def __init__(self, stream=sys.stderr):
         self.stream = stream
+        self.last_line_width = 0
 
     def echo(self, chars):
-        print(chars, file=self.stream)
+        # Print a line without a trailing newline
+        print(chars, file=self.stream, end='', flush=True)
+        self.last_line_width = len(chars)
+
+    def _reset_line(self):
+        """Replaces current shell output with spaces, so that the line can be overwritten."""
+        print('\r' + ' ' * self.last_line_width, file=self.stream, end='', flush=True)
+
--- logmia/tests/test_sh.py
+++ logmia/tests/test_sh.py
@@ -7,4 +7,10 @@ def test_basic_print():
     out = 'Nature always wears the colors of the spirit.'
     buf = io.StringIO()
     Sh(stream=buf).echo(out)
-    assert out == buf.getvalue().strip()
+    assert out == buf.getvalue()
+
+
+def test_reset_line():
+    buf = io.StringIO()
+    Sh(stream=buf)._reset_line()
+    assert buf.getvalue() == '\r'
