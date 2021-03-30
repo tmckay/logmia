cf35e81739f90ab6eebbcbd6ee044ab85e8d6ad6
Taylor McKay <taylormckay@gmail.com>
2021-03-29 20:56:08 -0700

    Factor sh module

--- logmia/sh.py
+++ logmia/sh.py
@@ -10,11 +10,14 @@ class Sh:
         self.last_line_width = 0
 
     def echo(self, chars):
-        # Print a line without a trailing newline
-        print(chars, file=self.stream, end='', flush=True)
+        self._print(chars)
         self.last_line_width = len(chars)
+
+    def _print(self, chars):
+        # Print without a trailing newline
+        print(chars, file=self.stream, end='', flush=True)
 
     def _reset_line(self):
         """Replaces current shell output with spaces, so that the line can be overwritten."""
-        print('\r' + ' ' * self.last_line_width, file=self.stream, end='', flush=True)
+        self._print('\r' + ' ' * self.last_line_width)
 
